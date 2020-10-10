b5a80ae5606d0ff52362e57ad40be2235594a4cf
LiamHua <liamhuazai@gmail.com>
2020-10-10 16:40:10 +0800

    使用druid数据源

--- modules/user-manage/src/main/java/team/hld/UserApplication.java
+++ modules/user-manage/src/main/java/team/hld/UserApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
+import javax.annotation.sql.DataSourceDefinition;
+
 
 /**
  * @author liam
--- modules/user-manage/src/test/java/MyTest.java
+++ modules/user-manage/src/test/java/MyTest.java
@@ -1,18 +1,27 @@
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import team.hld.entity.User;
 import team.hld.mapper.UserMapper;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
 import java.util.List;
 
 /**
  * @author liam
  * @date 2020/10/8 21:09
  */
+@SpringBootTest
 public class MyTest {
+    @Autowired
+    DataSource dataSource;
     @Test
-    public void test() {
+    public void test() throws SQLException {
+        System.out.println(dataSource.getClass());
+
+        System.out.println(dataSource.getConnection());
 
     }
 }
